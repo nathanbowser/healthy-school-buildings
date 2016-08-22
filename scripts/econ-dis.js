227805ae301b8b22bbd66f0dab7bc20016ecbbe5
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2016-08-22 18:26:11 -0400

    Wip scripts

--- scripts/econ-dis.js
+++ scripts/econ-dis.js
@@ -11,11 +11,14 @@ function append (all) {
       var ulcs = d[6]
 
       if (d[0].indexOf('School Name') !== -1) {
-        d.push('CEP Economically Disadvantaged Rate')
+        d.push('Special Needs - Count')
+        d.push('Special Needs - Percentage')
         return next(null, d)
       }
+
       var rate = all[ulcs]
-      d.push(rate[3])
+      d.push(rate[4])
+      d.push(rate[5])
       next(null, d)
     }))
     .pipe(stringify({eof: false}))
@@ -23,8 +26,14 @@ function append (all) {
 }
 
 function add (next) {
-  fs.createReadStream(__dirname + '/../data/econ-dis.csv')
+  fs.createReadStream(__dirname + '/../data/special.csv')
     .pipe(csv({objectMode: true, columns: false}))
+    .pipe(es.map(function (d, next) {
+      if (d[2] === 'ALL GRADES') {
+        return next(null, d)
+      }
+      next()
+    }))
     .pipe(reduce(function (result, data) {
       data[0] += 0
       result[data[0]] = data
--- scripts/extract.js
+++ scripts/extract.js
@@ -8,33 +8,32 @@ fs.createReadStream(__dirname + '/../data/master.csv')
   .pipe(csv({objectMode: true, columns: false}))
   .pipe(es.map(function (d, next) {
     if (d[0].indexOf('School Name') !== -1) {
-      d.push('Sq. Ft. (Building)')
-      d.push('Sq. Ft. (Site)')
-      d.push('Sq. Ft. (Footprint)')
+      d.splice(13, 0, 'Replacement Cost [CRV]')
       return next(null, d)
     }
 
     fs.readFile(__dirname + '/../data/text/profile/' + d[6] + '.txt', function (err, data) {
-      // console.log(data)
       if (err) {
-        // console.log('oh no', err)
-        d.push.apply(d, ['', '', ''])
+        // d.push.apply(d, ['', '', ''])
         return next(null, d)
       }
 
       var f = data.toString().split('\n')
-        , building = f[f.findIndex(function (line) {
-                        return line.indexOf('S.F. (Building):') !== -1
-                      })].substring(17)
-        , site = f[f.findIndex(function (line) {
-                    return line.indexOf('S.F. (Site):') !== -1
-                  })].substring(13).split(' ')[0]
-        , footprint = f[f.findIndex(function (line) {
-                        return line.indexOf('S.F. (Footprint):') !== -1
-                      })].substring(18)
-      d.push(building)
-      d.push(site)
-      d.push(footprint)
+        , rc = f[f.findIndex(function (line) {
+                        return line.indexOf('Replacement Cost:') !== -1
+                      })].substring(19)
+        , cac = f[f.findIndex(function (line) {
+                        return line.indexOf('Condition Assessment Cost:') !== -1
+                      })].substring(28)
+
+      // console.log(rc)
+      d[12] = cac.replace(/,/g, '')
+      d.splice(13, 0, rc.replace(/,/g, ''))
+        // Condition Assessment Cost: $42,949,319
+        // Replacement Cost: $95,850,000
+      // d.push(building)
+      // d.push(site)
+      // d.push(footprint)
 
       next(null, d)
     })
