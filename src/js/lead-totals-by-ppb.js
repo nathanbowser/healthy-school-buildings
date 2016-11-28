304f6af88ad771178ebccb78d9b3db78313c1d68
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2016-11-27 19:07:35 -0500

    Adds totals by ppb

--- src/js/lead-totals-by-ppb.js
+++ src/js/lead-totals-by-ppb.js
@@ -16,96 +16,56 @@ module.exports = function () {
     , height = 900 // TODO Drive by lookup
     , s = d3.scale.threshold().domain([3, 5, 10, 15, 20])
                               .range(['Less than 3', '3 - 5', '5 - 10', '10 - 15', '15 - 20', 'More than 20'])
-    , center = {
-      x: width / 2,
-      y: height / 2
-    }
-    , damper = 0.102
     , svg = d3.select('#vis')
               .append('svg')
               .attr('width', width)
               .attr('height', height)
-    , bubbles = null
-    , nodes = []
-
-  var force = d3.layout.force()
-                       .size([width, height])
-                       .charge(function (d) {
-                         return -Math.pow(d.radius, 2.0) / 7
-                       })
-                       .gravity(-0.01)
-                       .friction(.9)
-
-  // Sizes bubbles based on their area instead of raw radius
-  var radiusScale = d3.scale.pow()
-                            .exponent(1)
-                            .range([20, 75])
+    , bubble = d3.layout.pack()
+                        .sort(null)
+                        .size([width, height])
+                        .padding(1.5)
+                        .value(function (d) {
+                          return d[range.node().value]
+                        })
 
   draw()
 
   function draw () {
-    function createNodes (data) {
-      var nodes = data.filter(function (d) {
-                         return d[range.node().value] > 0
-                       })
-                      .map(function (d) {
-                        d.id = d.year + '-' + d.ulcs
-                        d.radius = radiusScale(d[range.node().value])
-                        d.x = Math.random() * 900
-                        d.y = Math.random() * 800
-                        return d
-                      })
+    var _data = window.data.lead.filter(function (d) {
+                                  return d[range.node().value] > 0
+                                })
+      , node = svg.selectAll('.node')
+                  .data(bubble.nodes({children: _data})
+                              .filter(function (d) {
+                                return !d.children
+                              }))
+      , enter = node.enter()
+                    .append('g')
+                      .attr('class', 'node')
 
-      // sort them to prevent occlusion of smaller nodes.
-      nodes.sort(function (a, b) { return b.collected - a.collected })
+    enter.append('circle')
+         .attr('fill', '#beccae')
 
-      return nodes
-    }
+    node.exit().remove()
 
-    var max = d3.max(window.data.lead, function (d) {
-                  return d[range.node().value]
-                })
+    node.attr('transform', function (d) {
+          return 'translate(' + d.x + ',' + d.y + ')'
+        })
 
-    radiusScale.domain([0, max])
+    node.select('circle')
+        .attr('r', function (d) {
+          return d.r
+        })
 
-    nodes = createNodes(window.data.lead)
+    enter.append('text')
+         .attr('dy', '.3em')
+         .style('text-anchor', 'middle')
+         .style('pointer-events', 'none')
 
-    force.nodes(nodes)
-
-    bubbles = svg.selectAll('.bubble')
-                 .data(nodes, function (d) {
-                   return d.id
-                 })
-
-    bubbles.enter()
-           .append('circle')
-
-    bubbles.exit().remove()
-
-    bubbles.classed('bubble', true)
-           .attr('r', 0)
-           .attr('fill', '#beccae')
-           .attr('stroke', d3.rgb('beccae').darker())
-           .attr('stroke-width', 2)
-
-    // Fancy transition to make bubbles appear, ending with the
-    // correct radius
-    bubbles
-           .attr('r', function (d) { return d.radius })
-
-    function moveToCenter (alpha) {
-      return function (d) {
-        d.x = d.x + (center.x - d.x) * damper * alpha
-        d.y = d.y + (center.y - d.y) * damper * alpha
-      }
-    }
-
-    force.on('tick', function (e) {
-           bubbles.each(moveToCenter(e.alpha))
-                  .attr('cx', function (d) { return d.x })
-                  .attr('cy', function (d) { return d.y })
-         })
-         .start()
+    node.select('text')
+        .text(function (d) {
+          return d.name.substring(0, d.r / 4) + ' (' + d.ulcs + ')' //+ ' -- ' + d[range.node().value]
+        })
   }
 
 }
